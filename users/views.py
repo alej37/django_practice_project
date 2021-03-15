978ed472de4f49f192181bf2ee0bc57c151a1565
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2021-03-14 23:22:33 -0400

    started adding some fields to update a user

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 def register(request):
@@ -19,5 +19,11 @@ def register(request):
 
 @login_required
 def profile(request):
+  u_form = UserUpdateForm()
+  p_form = ProfileUpdateForm()
 
-  return render(request, 'users/profile.html')
+  context = {
+    'u_form': u_form,
+    'p_form': p_form
+  }
+  return render(request, 'users/profile.html', context)
